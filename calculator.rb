db9d5a993fec96f26e13d877ffae130871da9e0e
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-04-02 21:56:04 +0000

    add additional test cases

--- calculator.rb
+++ calculator.rb
@@ -6,4 +6,15 @@ class Calculator
   def subtract(a, b)
     a - b
   end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    a / b
+  end
+
+  def mod(a, b)
+    a % b
 end
--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -11,6 +11,22 @@ class TestCalculator < Minitest::Test
 
   def test_sub
     calculator = Calculator.new
-    assert_equal 4, calculator.subtract(2, 2), "Addition method failed"
+    assert_equal 4, calculator.subtract(2, 2), "Subtraction method failed"
   end
+
+  def test_mult
+    calculator = Calculator.new
+    assert_equal 4, calculator.multiply(2, 2), "Multiplication method failed"
+  end
+
+  def test_div
+    calculator = Calculator.new
+    assert_equal 2, calculator.divide(4, 2), "Division method failed"
+  end
+
+  def test_mod
+    calculator = Calculator.new
+    assert_equal 1, calculator.mod(5, 2), "Modulo method failed"
+  end
+  
 end
